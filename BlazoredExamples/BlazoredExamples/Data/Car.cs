c8c634291fcb613e48409b3f8f69a643da3cb271
Julio Espinosa <julio.i.espinosa@gmail.com>
2021-01-21 10:21:45 -0600

    example for each blazored lib

---
+++ BlazoredExamples/BlazoredExamples/Data/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazoredExamples.Data
+{
+    public class Car
+    {
+        public string CarName { get; set; }
+        public int Year { get; set; }
+    }
+}
